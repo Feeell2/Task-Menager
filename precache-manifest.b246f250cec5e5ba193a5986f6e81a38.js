516dca14724708e54a9c82a9e2bc2f0e43672d85
Feeell2 <tomek.filinski@gmail.com>
2020-01-06 18:49:42 +0100

    Updates

---
+++ precache-manifest.b246f250cec5e5ba193a5986f6e81a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c695c8d4c258e741756ed9cf77f2cd",
+    "url": "/Task-Menager/index.html"
+  },
+  {
+    "revision": "1d3ee1a952caf7108203",
+    "url": "/Task-Menager/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "6e66e4b8fd9f49f26fa3",
+    "url": "/Task-Menager/static/css/main.5dfd94f8.chunk.css"
+  },
+  {
+    "revision": "1d3ee1a952caf7108203",
+    "url": "/Task-Menager/static/js/2.c3958b34.chunk.js"
+  },
+  {
+    "revision": "d6859736b922c7aa5b832b492ed4bd2f",
+    "url": "/Task-Menager/static/js/2.c3958b34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e66e4b8fd9f49f26fa3",
+    "url": "/Task-Menager/static/js/main.f45abee5.chunk.js"
+  },
+  {
+    "revision": "9c0a1ecd6ff763ff4a88",
+    "url": "/Task-Menager/static/js/runtime-main.32608d18.js"
+  }
+]);
